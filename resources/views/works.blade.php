af86f6b7d51587e143d162e7582440e9faf9e22c
Mike Ottinger <mottinger@sofi.org>
2025-04-06 11:37:02 -0700

    code for livewire example

--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -3,18 +3,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Works</title>
+    @livewireStyles
 </head>
 <body>
     <h1>Works Go Here</h1>
     
     <section>
-
         @foreach ($works as $w)
         <div style="margin-top: 10px">
             <div>{{$w}}</div>
         </div>    
         @endforeach
     </section>
-
+    <section>
+        <livewire:counter />
+    </section>
+@livewireScripts
 </body>
 </html>
