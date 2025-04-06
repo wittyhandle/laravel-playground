afb52b8c356463e77afccf27cd0b9d55f16ee9ac
Mike Ottinger <mottinger@sofi.org>
2025-04-06 14:03:23 -0700

    Added table migrations and model definitions for owner, patient, and treatment

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Owner extends Model
 {
-    //
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
 }
--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Treatment extends Model
 {
-    //
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
